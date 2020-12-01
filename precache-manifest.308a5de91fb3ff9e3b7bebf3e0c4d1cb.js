a7295f2a0b222c4e12137a26c92892d04618c04a
Bogdan Yaroshchuk <b2092ua@gmail.com>
2020-12-01 17:17:58 +0200

    Updates

---
+++ precache-manifest.308a5de91fb3ff9e3b7bebf3e0c4d1cb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf9386980381ce2af2aa0b096a88dc2",
+    "url": "/Social-Network-React/index.html"
+  },
+  {
+    "revision": "198256c4e1ae6761ec50",
+    "url": "/Social-Network-React/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "3d7b28698f66d5d78255",
+    "url": "/Social-Network-React/static/css/main.06fee659.chunk.css"
+  },
+  {
+    "revision": "198256c4e1ae6761ec50",
+    "url": "/Social-Network-React/static/js/2.5db7a1b8.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Social-Network-React/static/js/2.5db7a1b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7b28698f66d5d78255",
+    "url": "/Social-Network-React/static/js/main.c38a44e9.chunk.js"
+  },
+  {
+    "revision": "eab772e0cc2a6b5ea9e1",
+    "url": "/Social-Network-React/static/js/runtime-main.42576c8d.js"
+  },
+  {
+    "revision": "03299aa20c25cb21d392aea37202161c",
+    "url": "/Social-Network-React/static/media/602271647.03299aa2.gif"
+  },
+  {
+    "revision": "6aa4f4ff53f33e763214f94f9dd2cafe",
+    "url": "/Social-Network-React/static/media/810.6aa4f4ff.svg"
+  },
+  {
+    "revision": "405f7b21d61f37105f023267a6da3246",
+    "url": "/Social-Network-React/static/media/index.405f7b21.jpeg"
+  },
+  {
+    "revision": "56c96440dd8f76a8e40b42b537820c80",
+    "url": "/Social-Network-React/static/media/like.56c96440.png"
+  }
+]);
